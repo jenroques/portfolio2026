2b5fdd2a6c57a65f9f18c11b7c42a626e735e741
Jennifer Roques <jenniferroques@gmail.com>
2026-02-14 09:10:10 -0600

    Config setup

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ErrorBoundary } from "react-error-boundary";
+
 import App from './App.tsx'
+import { ErrorFallback } from './ErrorFallback.tsx'
+
+import "./main.css"
+import "./styles/theme.css"
+import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
     <App />
-  </StrictMode>,
+   </ErrorBoundary>
 )
